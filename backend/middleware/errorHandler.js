940a6a2112e4ebf05d38b566c6f442fde7a4e356
goldedgelabs <goldedgelabs@gmail.com>
2025-12-10 18:07:26 +0300

    Delete backend directory

--- backend/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-export default (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.statusCode || 500).json({
-    message: err.message || 'Server Error',
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  });
-};
