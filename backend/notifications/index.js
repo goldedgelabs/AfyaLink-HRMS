6f8c6d4eba0dafebd9f447101efd48186dd86a98
goldedgelabs <goldedgelabs@gmail.com>
2025-12-10 22:16:21 +0300

    Update index.js

--- backend/notifications/index.js
+++ backend/notifications/index.js
@@ -1,18 +1,19 @@
-
 import { sendTwilioSMS } from './twilioClient.js';
-import { sendATSMS } from './africastalkingClient.js';
+import { sendSMS as sendATSMS } from './africastalkingClient.js';
 
 export const sendSMS = async ({ to, message }) => {
   const results = {};
 
+  // Twilio
   try {
     results.twilio = await sendTwilioSMS({ to, message });
   } catch (err) {
     results.twilio = { error: err.message };
   }
 
+  // Africa's Talking
   try {
-    results.africastalking = await sendATSMS({ to, message });
+    results.africastalking = await sendATSMS(to, message);
   } catch (err) {
     results.africastalking = { error: err.message };
   }
