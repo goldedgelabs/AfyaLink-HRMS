2c7532a4751653e9c514d77250d40abef703e679
goldedgelabs <goldedgelabs@gmail.com>
2025-12-10 18:48:41 +0300

    Update twilioClient.js

--- backend/notifications/twilioClient.js
+++ backend/notifications/twilioClient.js
@@ -1,8 +1,7 @@
-
 // Twilio placeholder - install twilio and set env vars
 module.exports = {
   sendSMS: async (to, message) => {
-    console.log('Twilio sendSMS placeholder', to, message);
+    console.log("Twilio sendSMS placeholder:", to, message);
     return { ok: true, to, message };
   }
 };
