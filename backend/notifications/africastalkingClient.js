893c322f441b8c52e46c9b4dcbc7990622ea6f78
goldedgelabs <goldedgelabs@gmail.com>
2025-12-10 18:53:13 +0300

    Update africastalkingClient.js

--- backend/notifications/africastalkingClient.js
+++ backend/notifications/africastalkingClient.js
@@ -1,7 +1,9 @@
 // Africa's Talking placeholder - install africastalking and set env vars
-module.exports = {
+const africasTalkingClient = {
   sendSMS: async (to, message) => {
     console.log("Africa's Talking sendSMS placeholder:", to, message);
     return { ok: true, to, message };
   }
 };
+
+export default africasTalkingClient;
