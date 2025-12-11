89f9606d6b4abd8c7dbc1b8e306091526d1e88be
goldedgelabs <goldedgelabs@gmail.com>
2025-12-11 15:30:13 +0300

    Delete backend directory

--- backend/middleware/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-export const permit = (...allowed) => {
-  return (req, res, next) => {
-    const { user } = req;
-    if (!user) return res.status(401).json({ message: 'Not authorized' });
-    if (allowed.includes(user.role)) return next();
-    return res.status(403).json({ message: 'Forbidden: insufficient role' });
-  };
-};
