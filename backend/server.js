fb8b0290ee17a28069e5aef6cf1adf31a3f94d5e
goldedgelabs <goldedgelabs@gmail.com>
2025-12-18 02:37:36 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 // =======================================================
 const allowedOrigins = [
   process.env.FRONTEND_URL,
-  'http://localhost:5173',
+  'https://afya-link-hrms-frontend-4.vercel.app',
 ].filter(Boolean);
 // =======================================================
 
