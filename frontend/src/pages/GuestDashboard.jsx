d391edd8aaf6bf5a2d2678511d8d296fbb50d263
goldedgelabs <goldedgelabs@gmail.com>
2025-12-17 05:43:08 +0300

    Create GuestDashboard.jsx

---
+++ frontend/src/pages/GuestDashboard.jsx
@@ -0,0 +1,9 @@
+export default function GuestDashboard() {
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>AfyaLink Demo</h1>
+      <p>You are viewing the system as a guest.</p>
+      <p>Actions are disabled.</p>
+    </div>
+  );
+}
