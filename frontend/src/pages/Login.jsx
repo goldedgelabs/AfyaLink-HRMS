4f8162d1cd0887bbee3b3d517e45d11c3737c5e5
goldedgelabs <goldedgelabs@gmail.com>
2025-12-17 03:29:35 +0300

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../utils/auth";
+import { useAuth } from "../utils/auth";
+
 
 export default function Login() {
   const [email, setEmail] = useState("admin@afya.test");
