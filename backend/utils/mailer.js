afd463b6baf5efa9a8ba174a27aa9de86a878b9c
goldedgelabs <goldedgelabs@gmail.com>
2025-12-18 01:31:18 +0300

    Update mailer.js

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -4,7 +4,7 @@ export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: false,
+    secure: false, // true only for port 465
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
