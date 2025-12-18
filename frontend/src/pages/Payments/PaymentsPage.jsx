27bf9ef94ad0682e99db4074674b69f6cbb6e2e8
goldedgelabs <goldedgelabs@gmail.com>
2025-12-19 01:19:55 +0300

    Update PaymentsPage.jsx

--- frontend/src/pages/Payments/PaymentsPage.jsx
+++ frontend/src/pages/Payments/PaymentsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useAuth } from "../../utils/auth";
+import { apiFetch } from "../utils/apiFetch";
 import { apiFetch } from "../../utils/auth";
 
 /**
