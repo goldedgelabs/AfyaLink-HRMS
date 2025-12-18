0e77d71773eabd4479f9ad00c2f9721c7bb16831
goldedgelabs <goldedgelabs@gmail.com>
2025-12-19 01:47:14 +0300

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,6 @@
-// Redirect all imports from "api" to "apiFetch"
+// Placeholder to redirect imports from "api" to apiFetch
+export * from './apiFetch';
+
+// Optional default export for imports like `import API from '../utils/api'`
 import * as api from './apiFetch';
 export default api;
-
-export * from './apiFetch';
