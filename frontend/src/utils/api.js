2fa87ee62d941ddde5d37f4ed9e6e3588e34bfca
goldedgelabs <goldedgelabs@gmail.com>
2025-12-19 03:11:19 +0300

    Delete frontend/src/utils/api.js

--- frontend/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-// src/utils/api.js
-// Redirect all old api imports to apiFetch.js
-
-export * from './apiFetch';
-
-// Optional default export for imports like `import API from '../utils/api'`
-import * as api from './apiFetch';
-export default api;
