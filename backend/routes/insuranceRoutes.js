c45f634a4b846a3499a964dd8fa67e85d93d0bd7
goldedgelabs <goldedgelabs@gmail.com>
2025-12-22 10:43:39 +0300

    Update insuranceRoutes.js

--- backend/routes/insuranceRoutes.js
+++ backend/routes/insuranceRoutes.js
@@ -4,7 +4,8 @@ import {
   adminApproveInsurance,
   adminRejectInsurance,
 } from "../controllers/insuranceController.js";
-import { requireAuth } from "../middleware/authMiddleware.js";
+import protect from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 
